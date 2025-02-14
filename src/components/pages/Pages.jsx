77642763bd661d2ad12829946fe08fa00d143589
Esteban Espinoza <estebandesarrolladordesoftware@gmail.com>
2025-02-13 20:26:57 -0500

    a

--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import { HashRouter as Router, Route, Switch } from "react-router-dom"
 import Footer from "../common/Footer"
 import { Header } from "../common/Header"
 import { Home } from "../home/Home"
@@ -9,22 +9,21 @@ import { Contact } from "./Contact"
 import { Portfolio } from "./Portfolio"
 import { Services } from "./Services"
 import { Testimonials } from "./Testimonials"
+
 export const Pages = () => {
   return (
-    <>
-      <Router>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/about' component={About} />
-          <Route exact path='/services' component={Services} />
-          <Route exact path='/portfolio' component={Portfolio} />
-          <Route exact path='/testimonials' component={Testimonials} />
-          <Route exact path='/blog' component={Blog} />
-          <Route exact path='/contact' component={Contact} />
-        </Switch>
-        <Footer />
-      </Router>
-    </>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/about' component={About} />
+        <Route exact path='/services' component={Services} />
+        <Route exact path='/portfolio' component={Portfolio} />
+        <Route exact path='/testimonials' component={Testimonials} />
+        <Route exact path='/blog' component={Blog} />
+        <Route exact path='/contact' component={Contact} />
+      </Switch>
+      <Footer />
+    </Router>
   )
 }
